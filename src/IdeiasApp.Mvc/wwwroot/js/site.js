196ce9576ab3aeafe74a641915052d661533c9e9
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-21 18:12:36 -0300

    Add intructions in site.cs

--- src/IdeiasApp.Mvc/wwwroot/js/site.js
+++ src/IdeiasApp.Mvc/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+// Usar este arquivo para customizar meus layouts fora da template
