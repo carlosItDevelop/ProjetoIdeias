6370fd76fb3725addc66a65a0bf8066ceb35e924
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-22 15:24:48 -0300

    Criei o Enum StatusIdeia

---
+++ src/Cooperchip.IdeiasApp.Domain/Enums/StatusIdeia.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel;
+
+namespace Cooperchip.IdeiasApp.Domain.Enums
+{
+    public enum StatusIdeia
+    {
+        [Description("Produto Novo")]
+        Nova = 1,
+        [Description("Produto Existente")]
+        Existente
+    }
+}
