b4be5c6308880bf82508137fb80b737fa1c59b0c
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-23 07:48:38 -0300

    Modelo Arquivo criado

--- src/Cooperchip.IdeiasApp.Domain/Models/Arquivo.cs
+++ src/Cooperchip.IdeiasApp.Domain/Models/Arquivo.cs
@@ -1,6 +1,11 @@
-﻿namespace Cooperchip.IdeiasApp.Domain.Models
+﻿using System;
+using Cooperchip.IdeiasApp.DomainCore.Base;
+
+namespace Cooperchip.IdeiasApp.Domain.Models
 {
-    public class Arquivo
+    public class Arquivo : EntityBase
     {
+        public string Nome { get; set; }
+        public Byte[] ArquivoConteudo { get; set; }
     }
 }
