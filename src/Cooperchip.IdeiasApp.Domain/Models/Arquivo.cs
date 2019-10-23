bdf1e9a48eade8ba25060ef9555cb6cd4817d74a
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-23 07:56:24 -0300

    Na tabela Arqui a property Byte[] foi simplificada por byte[]

--- src/Cooperchip.IdeiasApp.Domain/Models/Arquivo.cs
+++ src/Cooperchip.IdeiasApp.Domain/Models/Arquivo.cs
@@ -6,6 +6,6 @@ namespace Cooperchip.IdeiasApp.Domain.Models
     public class Arquivo : EntityBase
     {
         public string Nome { get; set; }
-        public Byte[] ArquivoConteudo { get; set; }
+        public byte[] ArquivoConteudo { get; set; }
     }
 }
