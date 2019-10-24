b675e87f12cc2947e840ffb2d69b992a1ed814b3
Carlos A Santos <carlos.itdeveloper@gmail.com>
2019-10-24 04:49:49 -0300

    Instalação e Configuração do da Template Inspinia 2.9.2 com .Net Core 3.0 no projeto Cooperchip.Ideias.Mvc, com Identity e Config de Base de Dados

---
+++ src/Cooperchip.Ideias.Mvc/Controllers/GridOptionsController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cooperchip.Ideias.Mvc.Controllers
+{
+    public class GridOptionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
